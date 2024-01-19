fce3ad5d1f6bd0d21739d31d2f69f0914a48bd79
noydch <noydch2003@gmail.com>
2024-01-19 18:27:05 +0700

    App v1

---
+++ resources/js/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1 class="text-center">Hello Vue</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Minipos14App',
+
+    data() {
+        return {
+
+        };
+    },
+
+    mounted() {
+
+    },
+
+    methods: {
+
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
