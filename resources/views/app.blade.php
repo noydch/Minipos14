a206c17a979e5781376b82104b11ca74ea346d49
noydch <noydch2003@gmail.com>
2024-01-19 18:49:59 +0700

    App v2.0.0

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,9 @@
 </head>
 
 <body>
-    <h1>Hello Laravel</h1>
+    <h1>Hello Laravel v2.0.0</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quo sapiente ea commodi dolorum omnis illum ipsa
+        fugit non provident nostrum quasi voluptatibus, facere mollitia optio? Placeat impedit et temporibus.</p>
 
     <hr>
 
